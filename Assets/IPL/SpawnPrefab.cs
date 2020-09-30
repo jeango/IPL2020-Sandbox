7debcc4792feb748b569f203badcef7c2d4e1e2a
Jean-Gobert de Coster <jeangodecoster@outlook.com>
2020-09-30 12:48:32 +0200

    Cours 5

---
+++ Assets/IPL/SpawnPrefab.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace IPL
+{
+    public class SpawnPrefab : MonoBehaviour
+    {
+        [SerializeField] private GameObject prefab;
+
+        private Transform _transform;
+
+        private void Awake()
+        {
+            _transform = transform;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Instantiate(prefab, _transform.position, _transform.rotation);
+        }
+
+    }
+}
